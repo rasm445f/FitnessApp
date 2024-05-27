49123f37b4809e4daba629f3853d0969eb02abd5
rasm445f <90387951+rasm445f@users.noreply.github.com>
2024-05-27 12:56:01 +0200

    pagination for exercise page

--- src/components/ExerciseScreen.jsx
+++ src/components/ExerciseScreen.jsx
@@ -5,17 +5,18 @@ import {
   Text,
   View,
   ActivityIndicator,
+  Button,
 } from "react-native";
 import ExerciseListItem from "../utils/ExerciseListItem";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { gql } from "graphql-request";
 import client from "../graphqlClient";
 import { useAuth } from "../auth/AuthContext";
 import { Redirect } from "expo-router";
 
 const exercisesQuery = gql`
-  query exercises($muscle: String, $name: String) {
-    exercises(muscle: $muscle, name: $name) {
+  query exercises($muscle: String, $name: String, $offset: Int) {
+    exercises(muscle: $muscle, name: $name, offset: $offset) {
       name
       muscle
       equipment
@@ -24,12 +25,23 @@ const exercisesQuery = gql`
 `;
 
 export default function ExerciseScreen() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["exercises"],
-    queryFn: () => client.request(exercisesQuery),
-  });
+  const { data, isLoading, error, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["exercises"],
+      queryFn: ({ pageParam }) =>
+        client.request(exercisesQuery, { offset: pageParam }),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, pages) => pages.length * 10,
+    });
 
   const { username } = useAuth();
+
+  const loadMore = () => {
+    if (isFetchingNextPage) {
+      return;
+    }
+    fetchNextPage();
+  };
 
   if (isLoading) {
     return <ActivityIndicator />;
@@ -44,14 +56,18 @@ export default function ExerciseScreen() {
     return <Redirect href={"/auth"} />;
   }
 
+  const exercises = data?.pages.flatMap((page) => page.exercises);
+
   return (
     <View style={styles.container}>
       <Text>{username}</Text>
       <FlatList
-        data={data?.exercises}
+        data={exercises}
         contentContainerStyle={{ gap: 5 }}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => <ExerciseListItem item={item} />}
+        onEndReachedThreshold={1}
+        onEndReached={loadMore}
       />
       <StatusBar style="auto" />
     </View>
